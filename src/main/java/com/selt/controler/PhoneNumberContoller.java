4f84b4cd9902ed80005bd83c8306a82f99006625
Pawel <teksako@op.pl>
2023-01-04 15:33:56 +0100

    fix bugs

--- src/main/java/com/selt/controler/PhoneNumberContoller.java
+++ src/main/java/com/selt/controler/PhoneNumberContoller.java
@@ -66,7 +66,7 @@ public class PhoneNumberContoller {
     public String deletePhoneNumber(@PathVariable(value = "id") long id) {
         getAllNumbers();
         numberService.delete(id);
-        return "redirect:/list-departments";
+        return "redirect:/list-phoneNumbers";
     }
 
 }
